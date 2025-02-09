be19bb44aaa089fe3e046ccb2af68bf674b5ce69
edrivvoll <espendr@outlook.com>
2025-02-09 23:23:00 +0100

    prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,8 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: { ...globals.browser, module: true }  }},
+  { languageOptions: { globals: { ...globals.browser, module: true } } },
   pluginJs.configs.recommended,
 ];
