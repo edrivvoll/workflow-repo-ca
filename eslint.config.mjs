e256ebe74e804f3be4c2f30fdafd0ce07416de98
edrivvoll <espendr@outlook.com>
2025-02-14 21:43:52 +0100

    added globals to eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,5 +4,16 @@ import pluginJs from "@eslint/js";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   pluginJs.configs.recommended,
-  { languageOptions: { globals: { ...globals.browser, module: true, global: true } } },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        module: true,
+        global: true,
+        test: true,
+        it: true,
+        expect: true,
+      },
+    },
+  },
 ];
